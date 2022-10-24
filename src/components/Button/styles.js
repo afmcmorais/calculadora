3f88c6bf1b9bbf83e3e44dea61a990b8dd24fa9d
André Felipe <afmcmorais@icloud.com>
2022-10-24 07:03:25 -0300

    firts commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #03A4FE;
+    background-color: #C5DBFF;
+    color: #000000;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #081541;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #081541;
+        border: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        font-size: 24px;
+        color: #FFFFFF;
+    }
+`
